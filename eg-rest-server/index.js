c5930757c7f9a415b352a145c3945391476e7c12
david-mbp <davidroyseaman@gmail.com>
2022-11-29 18:55:21 +1300

    Update redis to stop those github warnings

--- eg-rest-server/index.js
+++ eg-rest-server/index.js
@@ -1,21 +1,18 @@
 const redis = require('redis');
 const express = require('express');
 const bodyParser = require('body-parser')
-const util = require('util');
 const uuidv4 = require('uuid/v4')
 
 // Format:
 // [redis[s]:]//[[user][:password@]][host][:port][/db-number][?db=db-number[&password=bar[&option=value]]]
 
 const client = redis.createClient('redis://redis-master:6379');
+const init = async () => {
+  await client.connect();
+  await client.select(1);
+}
 
-const rset = util.promisify(client.set).bind(client);
-const rget = util.promisify(client.get).bind(client);
-
-const rsadd = util.promisify(client.sadd).bind(client);
-const rsmembers = util.promisify(client.smembers).bind(client);
-
-client.select(1);
+init();
 // client.keys('*', (...args) => console.log({ args }));
 
 const rawParser = bodyParser.raw({
@@ -38,14 +35,14 @@ app.use((req, res, next) => {
 
 app.get(collection, async (req, res) => {
   console.log(`get collection ${req.url}`);
-  const data = await rsmembers(req.url);
+  const data = await client.smembers(req.url);
   res.send(JSON.stringify(data));
 });
 
 app.get(resource, async (req, res) => {
   console.log(`get resource ${req.url}`);
   // await new Promise((resolve) => setTimeout(resolve, 10));
-  const data = await rget(req.url);
+  const data = await client.get(req.url);
   res.send(data);
 });
 
@@ -53,14 +50,14 @@ app.get(resource, async (req, res) => {
 app.post(collection, async (req, res) => {
   console.log(`posted to collection ${req.url}`);
   const id = uuidv4();
-  await rsadd(req.url, id);
-  await rset(req.url + id, req.body.toString('utf8'));
+  await client.sadd(req.url, id);
+  await client.set(req.url + id, req.body.toString('utf8'));
   res.send(id);
 });
 
 app.put(resource, async (req, res, next) => {
   console.log(`put to resource ${req.url}`);
-  await rset(req.url, req.body);
+  await client.set(req.url, req.body);
   res.send('k');
 });
 
@@ -68,7 +65,7 @@ app.put(resource, async (req, res, next) => {
 
 
 
-app.listen(8080, (err) => {
+app.listen(8083, (err) => {
   if (err) { throw err; }
   console.log('server ready on http://localhost:8080')
 })
